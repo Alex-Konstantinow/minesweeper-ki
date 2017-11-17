c06b4c5d44ed284d88f4dd56fec2922d0b6469b4
Rebecca Armbruster <rarmbrus@mail.uni-mannheim.de>
2017-11-17 12:49:35 +0100

    KnowledgeBase skizziert und grundlegende Vorgehensweise implementiert

---
+++ src/de/unima/info/ki/minesweeper/api/KnowledgeBase.java
@@ -0,0 +1,19 @@
+package de.unima.info.ki.minesweeper.api;
+
+//SAT4J muss noch importiert werden
+
+public class KnowledgeBase {
+
+    //private ArrayList<Clause> clauses = new ArrayList<Clause>();
+
+    //Diese Methode fügt eine neue Klausel zur KnowledgeBase hinzu
+    /*public void addClause(Clause c) {
+        this.clauses.add(c);
+    }*/
+
+    //Diese Methode gibt die Klausel mit dem Index i zurück
+    /*public Clause getClause(index i) {
+
+    }*/
+
+}
